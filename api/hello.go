d48abd2e3e65ac7fb379fea0f36b5d5b19440909
lixuehan <lixuehan@tantanapp.com>
2022-06-06 21:16:00 +0800

    Hello

---
+++ api/hello.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	bot, err := tgbotapi.NewBotAPI("5465060326:AAGXybvWcExpT-RolQenge3PbVcJQx-mVm0")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	chatID := os.Getenv("CHAT_ID")
+	chatIDValue, _ := strconv.ParseInt(chatID, 10, 64)
+	msg := tgbotapi.NewMessage(chatIDValue, "你好呀")
+
+	if _, err := bot.Send(msg); err != nil {
+		log.Panic(err)
+	}
+
+	currentTime := time.Now().Format(time.RFC850)
+	fmt.Fprintf(w, currentTime)
+}
